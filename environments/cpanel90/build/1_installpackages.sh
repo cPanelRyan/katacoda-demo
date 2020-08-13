d91044c89d5247b9477b5d681adbc9fb54ce8b10
Ryan Robson <ryan.robson@cpanel.net>
2020-08-13 05:07:48 -0500

    modified installer to step out process and reduce mem reqs

--- environments/cpanel90/build/1_installpackages.sh
+++ environments/cpanel90/build/1_installpackages.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-cd /home && curl -o latest -L https://securedownloads.cpanel.net/latest && sh latest --force
+cd /home
+curl -o latest -L https://securedownloads.cpanel.net/latest
+sh latest --noexec --keep
+cd installd
+sed -i 's/768/512/' install
+sed -i 's/1_024,/512/' install
+./bootstrap
