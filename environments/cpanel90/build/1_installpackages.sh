973244e29c40d3fb6f0ae689262a9b13d9c7fbbc
Ryan Robson <ryan.robson@cpanel.net>
2020-08-13 05:17:21 -0500

    added hostname setting to deployment

--- environments/cpanel90/build/1_installpackages.sh
+++ environments/cpanel90/build/1_installpackages.sh
@@ -4,5 +4,6 @@ curl -o latest -L https://securedownloads.cpanel.net/latest
 sh latest --noexec --keep
 cd installd
 sed -i 's/768/512/' install
-sed -i 's/1_024,/512/' install
+sed -i 's/1_024,/512,/' install
+hostname "cp$RANDOM.kata.cpanel.net"
 ./bootstrap
