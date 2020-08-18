fc7edc0621a42c1c817dc7efb373e517e1ce64ea
Ryan Robson <ryan.robson@cpanel.net>
2020-08-18 03:56:38 -0500

    testing

--- environments/cpanel90/build/1_installpackages.sh
+++ environments/cpanel90/build/1_installpackages.sh
@@ -8,13 +8,13 @@ sed -i 's/1_024,/512,/' install
 hostname "cp$RANDOM.kata.cpanel.net"
 yum clean all
 yum makecache
-yum -y update
-systemctl stop NetworkManager
-systemctl disable NetworkManager 
-echo "NM_CONTROLLED=no" >> /etc/sysconfig/network-scripts/ifcfg-eth0
-echo "ONBOOT=yes" >> /etc/sysconfig/network-scripts/ifcfg-eth0
-echo "NM_CONTROLLED=no" >> /etc/sysconfig/network-scripts/ifcfg-lo
-echo "ONBOOT=yes" >> /etc/sysconfig/network-scripts/ifcfg-lo
-systemctl enable network.service
-systemctl start network.service
+#yum -y update
+#systemctl stop NetworkManager
+#systemctl disable NetworkManager 
+#echo "NM_CONTROLLED=no" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+#echo "ONBOOT=yes" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+#echo "NM_CONTROLLED=no" >> /etc/sysconfig/network-scripts/ifcfg-lo
+#echo "ONBOOT=yes" >> /etc/sysconfig/network-scripts/ifcfg-lo
+#systemctl enable network.service
+#systemctl start network.service
 ./bootstrap
