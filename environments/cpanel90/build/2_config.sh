9316625a107a953a113b2b7f4648b1eca07172dd
Ryan Robson <ryan.robson@cpanel.net>
2020-08-18 05:13:16 -0500

    extra configs

--- environments/cpanel90/build/2_config.sh
+++ environments/cpanel90/build/2_config.sh
@@ -2,4 +2,9 @@
 sed -i 's/alwaysredirecttossl=1/alwaysredirecttossl=0/' /var/cpanel/cpanel.config
 sed -i 's/requiressl=1/requiressl=0/' /var/cpanel/cpanel.config
 sed -i 's/strict/disabled/' /var/cpanel/cpanel.config
+touch /etc/.whostmgrft
+mkdir -p /var/cpanel/activate
+touch /var/cpanel/activate/2012-07.v01.EULACPWHM
+mkdir -p /var/cpanel/activate/features
+touch /var/cpanel/activate/features/disable_feature_showcase
 /scripts/restartsrv_cpsrvd
